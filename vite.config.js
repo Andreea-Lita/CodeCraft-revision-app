b742eb165d73088ad137df435ed2044f98b53930
Andreea-Lita <litaandreeamalaiasi@gmail.com>
2024-03-21 17:36:02 +0000

    app structure for beggin codecraft

--- vite.config.js
+++ vite.config.js
@@ -3,19 +3,19 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: "build"
-  },
-  css: {
-    modules: {
-      localsConvention: "camelCase"
-    }
-  },
-  server: {
-    hmr: {
-      overlay: false,
+    plugins: [react()],
+    build: {
+        outDir: "build"
     },
-  },
-  base: "/revision-app/"
+    css: {
+        modules: {
+            localsConvention: "camelCase"
+        }
+    },
+    server: {
+        hmr: {
+            overlay: false,
+        },
+    },
+    // base: "/revision-app/"
 });
